ddc4a8bc10aa7622fceb2ce717eb7c4654bab251
ericmutton <ericmutton@protonmail.com>
2025-08-14 05:52:01 -0500

    chore: correctly mark pin shutdown as active low

--- Software/radio-firmware/src/pam8302a.cpp
+++ Software/radio-firmware/src/pam8302a.cpp
@@ -1,4 +1,4 @@
-#include "pam8302a.h";
+#include "pam8302a.h"
 #include "pcf8575.h"
 
 extern pam8302a_config_t pam8302a;
@@ -6,7 +6,7 @@ extern pam8302a_config_t pam8302a;
 int pam8302a_init(pam8302a_config_t &configuration) {
   pam8302a = configuration;
 
-  pcf8575_writePort(pam8302a.pin_shutdown, HIGH); // active high
+  pcf8575_writePort(pam8302a.pin_shutdown, HIGH); // active low
 
   return 0;
 }
